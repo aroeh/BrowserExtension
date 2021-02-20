0e673c2ec03764d49bb204077536ede5b8f48eda
Adam Roehrig <arroeh01@outlook.com>
2021-02-20 11:02:18 -0500

    reduced image size and added and updated some of the functions in the popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,43 @@
+const content = document.getElementById("popupcontent");
 const sendMessageId = document.getElementById("sendmessageid");
 if (sendMessageId) {
     sendMessageId.onclick = function() {
+        let image = document.createElement("img");
+        image.src = "/images/Racheal-21Birthday.jpg";
+        image.id = `${guidGenerator()}`;
+        image.classList.add("slide-image");
+        document.body.appendChild(image);
+
+        image.onclick = function() {
+            image.remove();
+        }
+
+        function guidGenerator() {
+            const S4 = function () {
+                return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+            };
+            return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+        }
+    };
+}
+
+const tabUrl = document.getElementById("taburl");
+if (tabUrl) {
+    tabUrl.onclick = function() {
+        let txt = document.createElement("p");
+        txt.id = `${guidGenerator()}`;
+
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {
-                    url: chrome.extension.getURL("images/Racheal-21Birthday.jpg"),
-                    imageDivId: `${guidGenerator()}`,
-                    tabId: tabs[0].id
-                },
-                function(response) {
-                    window.close();
-                }
-            );
-            function guidGenerator() {
-                const S4 = function () {
-                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                };
-                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-            }
+            txt.innerHTML = "Currently viewing " + tabs[0].url;
         });
+
+        document.body.appendChild(txt);
+        
+        function guidGenerator() {
+            const S4 = function () {
+                return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+            };
+            return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+        }
     };
 }
